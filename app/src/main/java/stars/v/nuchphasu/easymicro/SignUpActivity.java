71bcae85f42c4c3cf51390e54c62c73011f4934a
friend <nuchphasu@gmail.com>
2016-11-05 15:57:17 +0700

    onActivityResult

--- app/src/main/java/stars/v/nuchphasu/easymicro/SignUpActivity.java
+++ app/src/main/java/stars/v/nuchphasu/easymicro/SignUpActivity.java
@@ -1,5 +1,6 @@
 package stars.v.nuchphasu.easymicro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -57,5 +58,29 @@ public class SignUpActivity extends AppCompatActivity {
             }// onClick
         });
 
+        //Image Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent to Other App
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "โปรดเลือกแอฟดูรูป"),0);
+            }//click
+        });//onclickListener
+
     }// Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
+            //Result Success
+            Log.d("5novV1", "Result OK");
+
+
+        }//if
+
+    }//onActivityResult
 }// Main Class
